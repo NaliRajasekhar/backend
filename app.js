0da7e8216ddcf9e91d1331bbf41bc7c8be53847a
Nali Rajasekhar <nalirajasekhar444@gmail.com>
2022-06-30 18:54:20 +0530

    backend

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 600);
+  res.status(err.status || 700);
   res.render('error');
 });
 
